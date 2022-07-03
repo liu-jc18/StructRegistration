619fdda42d9549966828bca6ba3a3265f77aa0be
liu-jc18 <liu-jc18@mails.tsinghua.edu.cn>
2022-07-03 16:50:07 +0800

    Add test data

--- examples/demo.cpp
+++ examples/demo.cpp
@@ -18,16 +18,33 @@ using namespace Eigen;
 
 int main( int argc, char** argv ){
 
-    if(argc != 3)
+    if(argc != 2)
     {
-        cerr << endl << "Usage: struct_reg path_to_target_pointcloud path_to_source_pointcloud" << endl;
+        cerr << endl << "Usage: struct_reg path_to_config" << endl;
         return 0;
     }  
 
-    std::string target_pcd = argv[1];
-    std::string source_pcd = argv[2];
+    YAML::Node node = YAML::LoadFile(argv[1]);
+    std::string base = node["base_path"].as<string>();
+    std::string TestNumber = node["TestNumber"].as<string>();
 
-    // target_cloud is only for visualization
+    std::string target_pcd, source_pcd;
+    target_pcd = base + "target.pcd";
+    source_pcd = base + TestNumber + ".pcd";
+
+    string GMMPath, FlagPath, PlanePath, GroundTruthPath;
+    GMMPath = base + "GMM.txt";
+    FlagPath = base + "PlaneFlag.txt";
+    PlanePath = base + "Planes.txt";
+    GroundTruthPath = base + TestNumber + ".txt";
+    
+    Eigen::Matrix4f gt;
+    LoadPoseGroundtruth(GroundTruthPath,gt);
+
+    cout<<"ground truth pose is "<<endl;
+    cout<<gt.inverse()<<endl;
+
+    // target_cloud is only used for visualization
     pcl::PointCloud<pcl::PointXYZ>::Ptr target_cloud(new pcl::PointCloud<pcl::PointXYZ>());
     pcl::PointCloud<pcl::PointXYZ>::Ptr source_cloud(new pcl::PointCloud<pcl::PointXYZ>());
 
@@ -40,29 +57,42 @@ int main( int argc, char** argv ){
         return 0;
     }
 
+    // Eigen::Matrix4f initPose;
+    // initPose.setIdentity();
+    // pcl::transformPointCloud(*source_cloud,*source_cloud,initPose);
 
-    // Load GMM
-    string GMMpath = "data/GMM_level_3.txt";
-    std::string Flagpath = "data/PlaneFlag.txt";
-    string Planepath = "data/planes.txt";
-    Register StructRegister(GMMpath,Flagpath,Planepath);
-
+    Register StructRegister(GMMPath,FlagPath,PlanePath);
     StructRegister.SetInputCloud(source_cloud);
+    
+    std::chrono::steady_clock::time_point t1 = std::chrono::steady_clock::now();  
     StructRegister.run();
+    std::chrono::steady_clock::time_point t2 = std::chrono::steady_clock::now();  
+    chrono::duration<double> time_12 = chrono::duration_cast<chrono::duration<double>>(t2-t1);
+    std::cout << "Time cost: " << time_12.count() << "[sec]" << std::endl;
 
     pcl::PointCloud<pcl::PointXYZ>::Ptr tf_clouds(new pcl::PointCloud<pcl::PointXYZ>());
     
     pcl::transformPointCloud(*source_cloud,*tf_clouds,StructRegister.mPose);
 
+    // Evaluate the accuracy
+    Eigen::Matrix4f ErrT = StructRegister.mPose * gt;
+    float Err_t = ErrT.block(0,3,3,1).norm();
+    float Err_angle = acos((ErrT.block(0,0,3,3).trace()-1)/2) * 180.0 / 3.1415926;
+    cout<<"Accuracy: \nErr_angle: "<<Err_angle<<endl<<"Err_t: "<<Err_t<<endl;
+
     // visualization
     pcl::visualization::PCLVisualizer vis("vis");
     vis.setBackgroundColor(255,255,255);
     pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> target_handler(target_cloud, 255.0, 0.0, 0.0);
-    pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> source_handler(tf_clouds, 0.0, 255.0, 0.0);
+    pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> source_handler(source_cloud, 0.0, 255.0, 0.0);
+    pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> result_handler(tf_clouds, 0.0, 0.0, 255.0);
+
     vis.addPointCloud(target_cloud, target_handler, "target");
-    vis.addPointCloud(tf_clouds, source_handler, "source");
+    vis.addPointCloud(source_cloud, source_handler, "source");
+    vis.addPointCloud(tf_clouds, result_handler, "result");
     vis.setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 3, "source");
     vis.setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 3, "target");
+    vis.setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 3, "result");
     vis.spin();
 
     return 0;
